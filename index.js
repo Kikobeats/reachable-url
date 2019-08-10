b7b30006762c489221eef4ce7b20424617684edc
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-08-10 13:47:29 +0200

    feat: prefix cdn urls

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 'use strict'
 
-const pAny = require('p-any')
+const isCdnUrl = require('is-cdn-url')
 const { URL } = require('url')
+const pAny = require('p-any')
 const got = require('got')
 
 const createRequest = method => async (url, opts) => {
@@ -21,6 +22,7 @@ const fromHEAD = createRequest('head')
 const fromGET = createRequest('get')
 
 module.exports = (url, opts = {}) => {
+  if (isCdnUrl(url)) url = `https:${url}`
   const { href: encodedUrl } = new URL(url)
   return pAny([fromHEAD(encodedUrl, opts), fromGET(encodedUrl, opts)])
 }
